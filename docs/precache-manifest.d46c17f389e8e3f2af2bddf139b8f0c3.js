00f04c02af9f0a41f6bd1d38725ee1fd6a35c738
cor <cor@pruijs.nl>
2020-05-28 19:43:33 +0200

    Deploy

---
+++ docs/precache-manifest.d46c17f389e8e3f2af2bddf139b8f0c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b488426b3602f27056b0fafc5b142aa",
+    "url": "/infogr-raytracer-webgl/404.html"
+  },
+  {
+    "revision": "7b222b38f1e19aae8374",
+    "url": "/infogr-raytracer-webgl/css/app.821394ac.css"
+  },
+  {
+    "revision": "17bb17f12fb0a115b1dcfe6493ffa201",
+    "url": "/infogr-raytracer-webgl/index.html"
+  },
+  {
+    "revision": "7c14e24d2ce019106b99",
+    "url": "/infogr-raytracer-webgl/js/about.f7b116dc.js"
+  },
+  {
+    "revision": "7b222b38f1e19aae8374",
+    "url": "/infogr-raytracer-webgl/js/app.eb6e13e1.js"
+  },
+  {
+    "revision": "6fb70a94f3d3cb96936a",
+    "url": "/infogr-raytracer-webgl/js/chunk-vendors.7cab0d61.js"
+  },
+  {
+    "revision": "144808dc56323f0dcbff51d3bc4d95c3",
+    "url": "/infogr-raytracer-webgl/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/infogr-raytracer-webgl/robots.txt"
+  }
+]);
